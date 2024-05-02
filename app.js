3e3f6bea0617b97313ce9cbfdf9687ddf5f8e43c
Aleksandar Stanchev <altryx15@gmail.com>
2024-05-02 16:41:35 +0300

    The movement is fully completed
    The player(frog) can move right,left,up,down if the arrows on the keyboard are pressed. The frog CAN'T escape the grid(or atleast I've not faced any errors at the moment

--- app.js
+++ app.js
@@ -2,10 +2,43 @@ const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result') // I'm looking for id = result
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div') // vsichki koito sa podchineni na grid (divcheta)  only the divs inside grid 
-let currentIndex = 0
-function moveFrog() {
-    
+let currentIndex = 76
+const width = 9;
 
+function moveFrog(e) { // pass-vame event
+    squares[currentIndex].classList.remove('frog')
+   
+            switch(e.key) {  // ako na eventa key-a e Arrow left -> izpulni neshto si tam... etc
+        
+                    case 'ArrowLeft' : 
+
+                    if (currentIndex % width !== 0) 
+                       currentIndex -= 1;
+                    break;
+
+                    case 'ArrowRight' : 
+
+                    if((currentIndex % width < width-1)) 
+                        currentIndex += 1
+                    
+                    break;
+
+                    case 'ArrowUp' : 
+
+                    if(currentIndex-width>=0) 
+                    currentIndex -= width
+                    break;
+
+                    case 'ArrowDown' : 
+
+                    if(currentIndex+width<=80)
+                        currentIndex += width
+                    break;
+            }
+            
+       
+     
+        
     squares[currentIndex].classList.add('frog')
 }
 
