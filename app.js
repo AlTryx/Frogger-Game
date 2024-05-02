ced3906de26d05e11af326ba4bd7d6929d47777a
Aleksandar Stanchev <altryx15@gmail.com>
2024-05-02 11:46:25 +0300

    Creating the frog

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result') // I'm looking for id = result
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div') // vsichki koito sa podchineni na grid (divcheta)  only the divs inside grid 
+let currentIndex = 0
+function moveFrog() {
+    
+
+    squares[currentIndex].classList.add('frog')
+}
+
+document.addEventListener('keyup', moveFrog)
