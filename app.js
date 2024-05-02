18289cf20d37d8dd4674e39b24c43afc367e25ab
Aleksandar Stanchev <altryx15@gmail.com>
2024-05-02 18:58:46 +0300

    The game's basics ARE FINISHED!

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const width = 9;
 const carsLeft = document.querySelectorAll('.car-left')
 const carsRight = document.querySelectorAll('.car-right')
 let timerId
+let outcomeTimerId
 let currentTime = 20
 
 function moveFrog(e) { // pass-vame event
@@ -48,7 +49,7 @@ function moveFrog(e) { // pass-vame event
     squares[currentIndex].classList.add('frog')
 }
 
-document.addEventListener('keyup', moveFrog)
+
 
 function autoMoveElements() {
     currentTime--
@@ -57,6 +58,10 @@ function autoMoveElements() {
     logsRight.forEach(logRight => moveLogRight(logRight))
     carsLeft.forEach(carLeft => moveCarLeft(carLeft))
     carsRight.forEach(carRight => moveCarRight(carRight))
+    
+}
+
+function checkOutComes() {
     lose()
     win()
 }
@@ -160,10 +165,12 @@ function lose()
 { 
     if(squares[currentIndex].classList.contains('c1') || 
     squares[currentIndex].classList.contains('l4') ||
-    squares[currentIndex].classList.contains('l5')) 
+    squares[currentIndex].classList.contains('l5')) ||
+    currentTime <= 0
     {
         resultDisplay.textContent = 'You lose!'
         clearInterval(timerId)
+        clearInterval(outcomeTimerId)
         squares[currentIndex].classList.remove('frog')
         document.removeEventListener('keyup', moveFrog)
     }
@@ -173,10 +180,24 @@ function win() {
     if(squares[currentIndex].classList.contains('ending-block')) {
         resultDisplay.textContent = 'You Win!'
         clearInterval(timerId)
-        
+        clearInterval(outcomeTimerId)
         document.removeEventListener('keyup', moveFrog)
     }
 }
 
-timerId = setInterval(autoMoveElements, 1000)
+startPauseButton.addEventListener('click', () => {
+    if(timerId) {
+        clearInterval(timerId)
+        clearInterval(outcomeTimerId)
+        outcomeTimerId = null
+        timerId = null
+        document.removeEventListener('keyup', moveFrog)
+    } else {
+        timerId = setInterval(autoMoveElements, 1000)
+        outcomeTimerId = setInterval(checkOutComes, 50) // prevent cheating :D
+        document.addEventListener('keyup', moveFrog)
+    }
+})
+
+
 
