05162001a0f89e634dae03678509ccfd5485ce72
Aleksandar Stanchev <altryx15@gmail.com>
2024-05-02 11:41:15 +0300

    App.Js
    I've created the app.js file.
    Inside of it I'm creating querySelectors for all the elements inside my Frogger game

---
+++ app.js
@@ -0,0 +1,4 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result') // I'm looking for id = result
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div') // vsichki koito sa podchineni na grid (divcheta)  only the divs inside grid 
